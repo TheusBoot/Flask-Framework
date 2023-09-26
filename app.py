9db7131ef001ed86e1c06b3d01b40d93d21cd720
Theusboot <emailmartingcf1@gmail.com>
2023-09-26 03:02:07 -0300

    templetes adicionado com sucesso

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def site():
-    return 'hello word'
+def index():
+    return render_template("index.html")
 
+
+@app.route('/sobre')
+def sobre():
+    return 'sobre'
 
